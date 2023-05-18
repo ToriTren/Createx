00ea43f707d75dfeeb9b170f46482af3ae674ec1
ToriTren <130772884+ToriTren@users.noreply.github.com>
2023-05-19 02:33:07 +0300

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,3 +7,13 @@ $('[data-fancybox]').fancybox({
     color : 'f00'
   }
 });
+
+$(function () {
+
+  var mixer = mixitup('.directions__list');
+
+$('.directions__filter-btn').on('click', function(){
+  $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+  $(this).addClass('directions__filter-btn--active')
+  })
+})
